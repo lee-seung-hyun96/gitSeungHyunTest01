7d4aff124d6c87cc237a351ce0bb3e097c4a2a92
Lee SeungHyun <Lee SeungHyun@DESKTOP-52A4FL1>
2021-03-12 12:16:00 +0900

    commit SHService

--- src/kosta/mvc/controller/HelloController.java
+++ src/kosta/mvc/controller/HelloController.java
@@ -4,4 +4,10 @@ public class HelloController {
 	public void aa() {
 		System.out.println(12345678);
 	}
+	public void seunghyun() {
+		System.out.println("충돌이 예상됩니다.");
+	}
+	public void wannaHaveLunch() {
+		System.out.println("점심을 먹고 싶어요 !");
+	}
 }
