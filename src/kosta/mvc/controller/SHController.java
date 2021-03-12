d620e737ef57e1cf16d6b40722fcc587029fe2b9
Lee SeungHyun <Lee SeungHyun@DESKTOP-52A4FL1>
2021-03-12 11:41:53 +0900

    커밋합니다

---
+++ src/kosta/mvc/controller/SHController.java
@@ -0,0 +1,7 @@
+package kosta.mvc.controller;
+
+public class SHController {
+	public void test() {
+		System.out.println("이승현_추가했습니다.");
+	}
+}
--- src/kosta/mvc/service/TestService.java
+++ src/kosta/mvc/service/TestService.java
@@ -1,5 +1,7 @@
 package kosta.mvc.service;
 
 public class TestService {
-
+	public void service() {
+		System.out.println("서비스 추가_이승현");
+	}
 }
