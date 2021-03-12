b9deaceb8a1844becaa00685859d10da2a1de61f
Subin <72715023+lightsaber29@users.noreply.github.com>
2021-03-12 12:17:39 +0900

    commit for crash

--- src/kosta/mvc/controller/HelloController.java
+++ src/kosta/mvc/controller/HelloController.java
@@ -4,4 +4,10 @@ public class HelloController {
 	public void aa() {
 		System.out.println(12345678);
 	}
+	public void bb() {
+		System.out.println("충돌이 일어나는 코드");
+	}
+	public void cc() {
+		System.out.println("메소드 추가");
+	}
 }
---
+++ src/kosta/mvc/service/SubService.java
@@ -0,0 +1,7 @@
+package kosta.mvc.service;
+
+public class SubService {
+	public void testname(){
+		System.out.println("충돌이 일어나지 않는 메소드");
+	}
+}
